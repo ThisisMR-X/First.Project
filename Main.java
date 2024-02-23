30a4aff6659377702b744b643132b5a5ab3e8afb
akram <akrammkozak@gmail.com>
2024-02-23 14:09:59 +0100

    Third Commit

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.print("Hello to my first project!");
         System.out.print("Hello to my first project2!");
+        System.out.print("Hello to my first project3!");
 
     }
 }
